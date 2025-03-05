dfa6bf6c83e2892895199f48ffa7defa95794e68
Dominik Lenhardt <dominik.lenhardt@outlook.de>
2025-03-05 23:31:59 +0100

    change files includes

---
+++ files/bashrc/ssh-agent.sh
@@ -0,0 +1,6 @@
+# Ansible managed file
+if [ -z "$SSH_AUTH_SOCK" ]
+then
+   eval $(ssh-agent) > /dev/null
+   ssh-add -l > /dev/null || alias ssh='ssh-add -l > /dev/null || ssh-add && unalias ssh; ssh'
+fi
