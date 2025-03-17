712088652488d34b71eac66a6ccdc54eabff6fff
Dominik Lenhardt <dominik.lenhardt@outlook.de>
2025-03-17 22:20:49 +0100

    start ssh-agent only on interactiv shells

--- files/bashrc/ssh-agent.sh
+++ files/bashrc/ssh-agent.sh
@@ -1,6 +1,12 @@
 # Ansible managed file
-if [ -z "$SSH_AUTH_SOCK" ]
-then
-   eval $(ssh-agent) > /dev/null
-   ssh-add -l > /dev/null || alias ssh='ssh-add -l > /dev/null || ssh-add && unalias ssh; ssh'
+
+# Prüfen, ob die Shell interaktiv ist
+if [[ $- == *i* ]]; then
+    if [ -z "$SSH_AUTH_SOCK" ]; then
+        # Prüfen, ob bereits ein ssh-agent läuft
+        if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+            eval $(ssh-agent) > /dev/null
+        fi
+        ssh-add -l > /dev/null || alias ssh='ssh-add -l > /dev/null || ssh-add && unalias ssh; ssh'
+    fi
 fi
